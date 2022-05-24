21c7caf34ffc321eb1be880f3160101e65534ad3
Robin Csutorás <robin.csutoras@supercharge.io>
2022-05-24 14:23:20 +0200

    feat: add webcamera setup

--- src/face-detector.ts
+++ src/face-detector.ts
@@ -3,7 +3,7 @@ import * as faceapi from "face-api.js";
 export class FaceDetector {
   private loaded = faceapi.loadSsdMobilenetv1Model("/");
 
-  public async detectFace(canvas: HTMLCanvasElement) {
+  public async detectFace(canvas: HTMLCanvasElement | HTMLVideoElement) {
     await this.loaded;
     const detection = await faceapi.detectSingleFace(canvas);
     return detection;
--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,45 @@ import "./style.css";
 import { Controller } from "./controller";
 import { FaceDetector } from "./face-detector";
 
+const drone = false;
+
 const controller = new Controller();
-
-controller.init();
-
 const faceDetector = new FaceDetector();
 
-const box = document.getElementById('box') as HTMLDivElement;
+const video = document.createElement("video");
+if (drone) {
+  controller.init();
+} else {
+  const videoFeed = document.getElementById("videoFeed");
+  videoFeed?.appendChild(video);
+
+  if (navigator.mediaDevices.getUserMedia) {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    video.autoplay = true;
+  }
+}
+
+const box = document.getElementById("box") as HTMLDivElement;
 
 const callback = async () => {
-    const detection = await faceDetector.detectFace(controller.h264_player.canvas);
-    if (detection) {
-        box.style.display = 'block';
-        box.style.top = `${detection.box.y}px`;
-        box.style.left = `${detection.box.x}px`;
-        box.style.width = `${detection.box.width}px`;
-        box.style.height = `${detection.box.height}px`;
-    } else {
-        box.style.display = 'none';
-    }
-    requestAnimationFrame(callback);
+  const detection = await faceDetector.detectFace(
+    drone ? controller.h264_player.canvas : video
+  );
+  if (detection) {
+    box.style.display = "block";
+    box.style.top = `${detection.box.y}px`;
+    box.style.left = `${detection.box.x}px`;
+    box.style.width = `${detection.box.width}px`;
+    box.style.height = `${detection.box.height}px`;
+  } else {
+    box.style.display = "none";
+  }
+  requestAnimationFrame(callback);
 };
+
+if (drone) {
 requestAnimationFrame(callback);
+} else {
+    video.onplay = callback;
+}
